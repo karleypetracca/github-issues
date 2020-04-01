f3bd2f022269a02d841b163a7b201ee734f55c1b
Karley Petracca <karleypetracca@gmail.com>
2020-04-01 11:49:08 -0400

    Fix: corrected use of class Link to normal a href

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import IssueList from "./components/IssueList.jsx";
-import IssueDetail from "./components/IssueDetail.jsx/index.js";
+import IssueDetail from "./components/IssueDetail.jsx";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Link to={"/"}>
-          <p>GitHub Issues</p>
-        </Link>
-      </header>
-      <Router>
-        <Route path="/" exact component={IssueList} />
-        <Route path="/issue/:number?" component={IssueDetail} />
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<a href="/">
+					<p>GitHub Issues</p>
+				</a>
+			</header>
+			<Router>
+				<Route path="/" exact component={IssueList} />
+				<Route path="/issue/:number?" component={IssueDetail} />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
